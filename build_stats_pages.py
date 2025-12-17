26acd06a5694646ea589a63afbbd7ba32846f31c
Arthur Blot <arthur.blot78@gmail.com>
2025-12-17 14:32:18 +0100

    Clean and move stuff around for the CSV version

--- build_stats_pages.py
+++ build_stats_pages.py
@@ -18,10 +18,9 @@ HTML_STATUS = [
     ("unknown", "orange", "unknown"),
     ("wrong", "red", "not matching the name hash"),
     # for obfuscated
-    ("nok_from_previous", "yellow", "obfuscated but matching a previous non-obfuscated name"),
-    ("nok_dubious", "brown", "obfuscated but found from an unknown source"),
-    ("unknown_nonobf", "orange", "unknown and non-obfuscated"),
-    ("unknown_obf", "grey", "unknown but obfuscated")
+    ("obf_ok", "yellow", "obfuscated but matching a previously known name"),
+    ("obf_unk", "orange", "unknown but non-obfuscated in a previous version"),
+    ("obf_new", "grey", "present only as obfuscated")
 ]
 
 def find_html_status(status):
@@ -71,10 +70,10 @@ def html_library(module, lib, stats_byver, versions):
     # Make statistics over all versions to give an overall % of resolution for the library
     status_bynid = {}
     for ver in stats_byver:
-        for status in stats_byver[ver][0]:
-            if status == "total":
+        for status in stats_byver[ver]:
+            if status == "total" or status == "is_obf":
                 continue
-            for cur_nid in stats_byver[ver][0][status]:
+            for cur_nid in stats_byver[ver][status]:
                 status_bynid[cur_nid["nid"]] = status
     cnt = Counter(status_bynid.values())
     both_stats = []
@@ -96,11 +95,9 @@ def html_library(module, lib, stats_byver, versions):
             output += "<td></td>"
             continue
         output += "<td>"
-        cur_stats = stats_byver[ver][0]
+        cur_stats = stats_byver[ver]
         # Add a star if the NIDs of that library were (re-)randomized in that firmware version
-        is_obf = stats_byver[ver][1]
-        if is_obf:
-            print("OBF", module, versions[versions.index(ver) - 1], ver)
+        if stats_byver[ver]["is_obf"]:
             obf_str = '<div style="position: absolute; width: 100%; height: 100%; text-align: center;">*</div>'
         else:
             obf_str = ''
@@ -177,28 +174,50 @@ Hover a cell to know the meaning of the color. <br />
     output += "</table></div></body></html>"
     return output
 
-# Build the statistics for a given library at a given version. "obfuscated" is specified if the NIDs of the library have been randomized in the current or a previous firmware version.
-# "prev_nonobf" lists the NIDs already seen in a version of the library were the NIDs were not (yet) randomized.
-# "prev_ok" lists all the NIDs for which a name was found (which corresponds to the NID when computing the hash).
-def make_stats(module, lib, version, obfuscated, cur_nids, prev_nonobf, prev_ok, nid_names, obfuscation_pairs):
+# Build the statistics for a given library at a given version.
+def make_stats(module, lib, version, cur_nids, nid_names, obfuscation_pairs):
     unk_nids = []
     nok_nids = []
     ok_nids = []
-    obf_nids = []
+    obf_ok_nids = []
+    obf_unk_nids = []
+    obf_new_nids = []
+    is_obf = False
     # Sort NIDs by category: unknown (name ends with the NID), ok (NID matches the hash) and nok (NID doesn't match the hash)
     for cur_nid in cur_nids:
         fullname = lib + '_' + cur_nid
+        # TODO: handle guesses
         if fullname in nid_names:
             if nid_names[fullname][1] == 'matching':
                 ok_nids.append({"nid": cur_nid, "name": nid_names[fullname][0], "source": nid_names[fullname][1]})
-            else: # TODO: handle guesses
+            else:
                 unk_nids.append({"nid": cur_nid, "name": nid_names[fullname][0], "source": nid_names[fullname][1]})
         else:
-            obf_nids.append({"nid": cur_nid, "name": fullname, "source": 'obfuscated'}) # TODO: differentiate cases where the previous NID is known or not, and if its name was found or not
+            is_obf = True
+            oldername = fullname
+            cycle_detect = [oldername]
+            while oldername in obfuscation_pairs:
+                oldername = obfuscation_pairs[oldername]
+                if oldername in nid_names: # used to avoid some cycles (NIDs which reappeared as non-obfuscated)
+                    break
+                if oldername in cycle_detect:
+                    print("cycle detected with", fullname)
+                    exit(1)
+            oldest_nid = oldername.split('_')[-1]
+            if oldername != fullname:
+                print(fullname, "->", oldername, "->", oldest_nid)
+            if oldername in nid_names:
+                if nid_names[oldername][1] == 'matching':
+                    obf_ok_nids.append({"nid": oldest_nid, "name": fullname, "source": "obf_matching"})
+                else:
+                    obf_unk_nids.append({"nid": oldest_nid, "name": fullname, "source": "obf_unknown"})
+            else:
+                obf_new_nids.append({"nid": oldest_nid, "name": fullname, "source": "obf_new"})
 
-    stats = {"known": ok_nids, "unknown": unk_nids, "wrong": nok_nids, "unknown_obf": obf_nids}
+    stats = {"known": ok_nids, "unknown": unk_nids, "wrong": nok_nids, "obf_ok": obf_ok_nids, "obf_unk": obf_unk_nids, "obf_new": obf_new_nids}
 
     stats['total'] = len(cur_nids)
+    stats['is_obf'] = is_obf
 
     return stats
 
@@ -212,25 +231,20 @@ def get_nids_ver(nids, ver):
 # Make statistics for all the versions of a library, write the single HTML page, and return the row for the main page
 def handle_library(module, lib, nids, versions, nid_names, obf_pairs):
     vers = list(sorted(set([v for nid in nids for v in nid["versions"]])))
-    # Indicates the NIDs had at least one round of randomization in previous (or current) firmware version
-    now_obfuscated = False
     prev_nonobf = {}
     prev_ok = {}
-    stats_byver = {vers[0]: (make_stats(module, lib, vers[0], now_obfuscated, get_nids_ver(nids, vers[0]), prev_nonobf, prev_ok, nid_names, obf_pairs), False)}
+    stats_byver = {vers[0]: make_stats(module, lib, vers[0], get_nids_ver(nids, vers[0]), nid_names, obf_pairs)}
     for (v1, v2) in zip(vers, vers[1:]):
         # For each consecutive firmware versions v1 and v2, see their respective NIDs
-        is_obfuscated = False # TODO
-        if is_obfuscated:
-            now_obfuscated = True
-        stats_byver[v2] = (make_stats(module, lib, v2, now_obfuscated, get_nids_ver(nids, v2), prev_nonobf, prev_ok, nid_names, obf_pairs), is_obfuscated)
+        stats_byver[v2] = make_stats(module, lib, v2, get_nids_ver(nids, v2), nid_names, obf_pairs)
 
     # Get the results by NID for the individual pages
     stats_bynid = defaultdict(dict)
     for v in vers:
-        for status in stats_byver[v][0]:
-            if status == "total":
+        for status in stats_byver[v]:
+            if status == "total" or status == "is_obf":
                 continue
-            for cur_nid in stats_byver[v][0][status]:
+            for cur_nid in stats_byver[v][status]:
                 stats_bynid[cur_nid["nid"]][v] = (status, cur_nid["name"], cur_nid["source"])
     with open(OUTPUT_HTML + '/modules/' + module + '_' + lib + '.html', 'w') as fd:
         fd.write(html_single_library(module, lib, stats_bynid, vers))
@@ -264,7 +278,7 @@ def main():
 
     obfuscation_pairs = {}
     for line in open('obfuscation_pairs.csv', 'r').read().split('\n')[:-1]:
-        obfuscation_pairs[line.split(',')[0]] = line.split(',')[1]
+        obfuscation_pairs[line.split(',')[1]] = line.split(',')[0]
 
     nid_names = {}
     for csv in glob.glob('PSPLibDoc/kd/*.csv') + glob.glob('PSPLibDoc/vsh/module/*.csv'):
